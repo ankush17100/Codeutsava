42c654acd9be7b57c3319a1463f6e0e02e0f8b4e
Aanshi Gupta <guptaaanshi9@gmail.com>
2019-02-17 01:52:41 +0530

    AANsh

--- Startup_description.php
+++ Startup_description.php
@@ -1,6 +1,29 @@
 <?php include('includes/header.php') ?>
+<style type="text/css">
+    .des{
+        color:navy;
+    }
+    .des em{
+        color:navy;
+        font-size:30px;
+        font-family:"Times New Roman";
+        background: linear-gradient(yellow,white);
 
-<div id="blue">
+    }
+    .desc{
+        color:navy;
+
+    }
+    .tr{
+        color:navy;
+        font-size:20px;
+        background:radial-gradient(white,grey);
+    }
+</style>
+  <!-- *****************************************************************************************************************
+     BLUE WRAP
+     ***************************************************************************************************************** -->
+  <div id="blue">
     <div class="container">
       <div class="row">
         <h3>Startup Profile</h3>
@@ -10,14 +33,167 @@
     <!-- /container -->
   </div>
   <!-- /blue -->
-  
-<div class="container mtb">
 
-<div class="image" style="Height:100px">
-    <img src="img1.png" height="100px" width="100px" >
-</div>
 
-</div>
+  <!-- *****************************************************************************************************************
+     AGENCY ABOUT
+     ***************************************************************************************************************** -->
+
+  <div class="container mtb">
+    <div class="row" style="background:radial-gradient(white,aqua,white,aqua,white,aqua)">
+      <div class="col-lg-12">
+        <h1 style="color:navy;font-family:Comic Sans MF;background:linear-gradient(yellow,white);" ><center>BlueBanyan Startup</center></h1>
+        <br>
+        <p><u><h4>About Us</h4></u></p>
+        <p style="color:navy;"><b>BlueBanyan is one of the fastest growing Software Development & Digital Marketing Company from Raipur. In a very short duration of time earned a very good reputation and confidence of their clients. Right from its inception BlueBanyan made new standards of its own in the market providing the best products and services to its customer.</b></p>
+        <br>
+        <p><u><h4>Founder's Name</h4></u></p>
+        <p class="des" > <b>The founder of this startup is <em>Puru Agrawal</em></b>.</p>
+        <p><u><h4>Years of the start of the startup</h4></u></p>
+        <p class="desc"><b>The startup started 4 years back in the year 2015.</p>
+        <p><u><h4>Startup Email-address</h4></u></p>
+        <p class="des"><b><a>info@gmail.com</a> : This Email address could be used for contact.</b></p>
+        <p><u><h4>Startup Contact Number</h4></u></p>
+        <p class="des"><b><a>(+91)737373-9653</a> : This Contact Number could be used for any queries.</b></p>
+        <p><u><h4>Expected Seed Fund</h4></u></p>
+        <p class="des"><b>The Expected seed fund is 2 lacs.</b></p>
+        <p><u><h4>Past Experience</h4></u></p>
+        <p class="des"><b>This includes the past experience of the startup.</b></p>
+        <p><br/><a href="contact.html" class="btn btn-link" style="background-color:yellow;"><b>See Mentor</b></a></p>
+      </div>
+    </div>
+  </div>
+
+  <!-- *****************************************************************************************************************
+     TEEAM MEMBERS
+     ***************************************************************************************************************** -->
+
+  <div class="container mtb">
+    <div class="row centered">
+      <h3 class="mb">MEET OUR TEAM</h3>
+
+      <div class="col-lg-3 col-md-3 col-sm-3">
+        <div class="he-wrap tpl6">
+          <img src="G:/xampp/htdocs/team01.jpg" alt="" height="100px" width="100px">
+          <div class="he-view">
+            <div class="bg a0" data-animate="fadeIn">
+              <h3 class="a1" data-animate="fadeInDown">Contact Me:</h3>
+              <a href="#" class="dmbutton a2" data-animate="fadeInUp"><i class="fa fa-envelope"></i></a>
+              <a href="#" class="dmbutton a2" data-animate="fadeInUp"><i class="fa fa-twitter"></i></a>
+            </div>
+            <!-- he bg -->
+          </div>
+          <!-- he view -->
+        </div>
+        <!-- he wrap -->
+        <h4>Mark Webber</h4>
+        <h5 class="ctitle">CEO</h5>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+        <div class="hline"></div>
+      </div>
+
+      <div class="col-lg-3 col-md-3 col-sm-3">
+        <div class="he-wrap tpl6">
+          <img src="team01.jpg" alt="">
+          <div class="he-view">
+            <div class="bg a0" data-animate="fadeIn">
+              <h3 class="a1" data-animate="fadeInDown">Contact Me:</h3>
+              <a href="#" class="dmbutton a2" data-animate="fadeInUp"><i class="fa fa-envelope"></i></a>
+              <a href="#" class="dmbutton a2" data-animate="fadeInUp"><i class="fa fa-twitter"></i></a>
+            </div>
+            <!-- he bg -->
+          </div>
+          <!-- he view -->
+        </div>
+        <!-- he wrap -->
+        <h4>Paul Jameson</h4>
+        <h5 class="ctitle">LEAD DESIGNER</h5>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+        <div class="hline"></div>
+      </div>
+
+      <div class="col-lg-3 col-md-3 col-sm-3">
+        <div class="he-wrap tpl6">
+          <img src="team01.jpg" alt="">
+          <div class="he-view">
+            <div class="bg a0" data-animate="fadeIn">
+              <h3 class="a1" data-animate="fadeInDown">Contact Me:</h3>
+              <a href="#" class="dmbutton a2" data-animate="fadeInUp"><i class="fa fa-envelope"></i></a>
+              <a href="#" class="dmbutton a2" data-animate="fadeInUp"><i class="fa fa-twitter"></i></a>
+            </div>
+            <!-- he bg -->
+          </div>
+          <!-- he view -->
+        </div>
+        <!-- he wrap -->
+        <h4>Laura Sommers</h4>
+        <h5 class="ctitle">LEAD DEVELOPER</h5>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+        <div class="hline"></div>
+      </div>
+
+      <div class="col-lg-3 col-md-3 col-sm-3">
+        <div class="he-wrap tpl6">
+          <img src="team01.jpg" alt="">
+          <div class="he-view">
+            <div class="bg a0" data-animate="fadeIn">
+              <h3 class="a1" data-animate="fadeInDown">Contact Me:</h3>
+              <a href="#" class="dmbutton a2" data-animate="fadeInUp"><i class="fa fa-envelope"></i></a>
+              <a href="#" class="dmbutton a2" data-animate="fadeInUp"><i class="fa fa-twitter"></i></a>
+            </div>
+            <!-- he bg -->
+          </div>
+          <!-- he view -->
+        </div>
+        <!-- he wrap -->
+        <h4>Martin Blunt</h4>
+        <h5 class="ctitle">MARKETING</h5>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+        <div class="hline"></div>
+      </div>
+
+    </div>
+  </div>
+
+  <!-- *****************************************************************************************************************
+     TESTIMONIALS
+     ***************************************************************************************************************** -->
+  <div id="twrap">
+    <div class="container centered">
+      <div class="row">
+        <div class="col-lg-8 col-lg-offset-2">
+          <i class="fa fa-comment-o"></i>
+          <p>Entrepreneur with a demonstrated history of working in the information technology and services industry. Juggling between startups that are in the process of making a dent to the industry and a running a full stack software development firm that is serving some of the biggest names in Government and Private sectors. Skilled in Negotiation, Public Speaking, and good at programming in Java. Strong entrepreneurship professional with a Bachelor of Engineering (BE) focused in Computer Science and a programmer at heart.</p>
+          <h4><br/>Puru Agrawal</h4>
+          <p>CEO BlueBanyan</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- *****************************************************************************************************************
+     OUR CLIENTS
+     ***************************************************************************************************************** -->
+  <div id="cwrap">
+    <div class="container">
+      <div class="row centered">
+        <h3>Top Services</h3>
+        <div class="col-lg-3 col-md-3 col-sm-3">
+          <p class="tr"><b>Website Development</b></p>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-3">
+          <p class="tr"><b>Digital Marketing</b></p>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-3">
+          <p class="tr"><b>Facebook Marketing</b></p>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-3">
+          <p class="tr"><b>PPR Advertising</b></p>
+        </div>
+      </div>
+    </div>
+  </div>
+
 
 
 <?php include('includes/footer.php') ?>
