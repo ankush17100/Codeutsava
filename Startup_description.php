cf7949ea7b33e51d007f0ac1b097931715eed066
Rajiv Nayan Choubey <rajivnayanc@gmail.com>
2019-02-17 02:56:12 +0530

    Rajiv

--- Startup_description.php
+++ Startup_description.php
@@ -1,9 +1,20 @@
 <?php include('includes/header.php') ?>
 
+<div id="blue">
+    <div class="container">
+      <div class="row">
+        <h3>Startup Profile</h3>
+      </div>
+      <!-- /row -->
+    </div>
+    <!-- /container -->
+  </div>
+  <!-- /blue -->
+  
+<div class="container mtb">
 
-<div class="container">
-
-<div class="image"><img src="img1.png" height="100px" width="100px">
+<div class="image" style="Height:100px">
+    <img src="img1.png" height="100px" width="100px" >
 </div>
 
 </div>
